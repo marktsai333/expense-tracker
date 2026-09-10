43b49e542d0d06345fe24daea9c99b3ada085f9b
marktsai333 <marktsai333@gmail.com>
2026-09-10 12:56:19 +0800

    Updates

--- firebase-config.js
+++ firebase-config.js
@@ -2,4 +2,11 @@
  * Paste the Firebase Web app config object here after creating the project.
  * This file is public by design; Firestore Security Rules protect the data.
  */
-window.__EXPENSE_TRACKER_FIREBASE_CONFIG__ = window.__EXPENSE_TRACKER_FIREBASE_CONFIG__ || null;
+window.__EXPENSE_TRACKER_FIREBASE_CONFIG__ = window.__EXPENSE_TRACKER_FIREBASE_CONFIG__ || {
+  apiKey: 'AIzaSyCmkGSorbNSvKAXXGvFQ0mzMkvb7wVAIlM',
+  authDomain: 'expense-tracker-e8a98.firebaseapp.com',
+  projectId: 'expense-tracker-e8a98',
+  storageBucket: 'expense-tracker-e8a98.firebasestorage.app',
+  messagingSenderId: '483639605713',
+  appId: '1:483639605713:web:9db5f46012416ad87848f0',
+};
